d1b2120f69f176470ddc2a9dfad24a85fae212e6
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-05-21 07:12:24 +0800

    up

---
+++ API/Tables/Applications.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Tables
+{
+    public class Applications
+    {
+        public int AppID { get; set; }
+        public string AppName { get; set; }
+        public string AppMemberName { get; set; }
+        public string AppUrl { get; set; }
+        public string AppSecurityKey { get; set; }
+    }
+}
---
+++ BW/Controllers/FrontendControllers/ApplicationsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BW.APIHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Tables;
+using Newtonsoft.Json;
+
+namespace BW.Controllers.FrontendControllers
+{
+    [Produces("application/json")]
+    [Route("api/Applications")]
+    public class ApplicationsController : Controller
+    {
+        private ApiServices _api;
+
+        [HttpGet]
+        public async Task<List<Applications>> Get()
+        {
+            _api = new ApiServices("applications", null);
+            return JsonConvert.DeserializeObject<List<Applications>>(await _api.Get());
+        }   
+    }
+}
